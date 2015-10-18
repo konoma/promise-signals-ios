09a4cb99d39023cf08babf85f883c68989d71a72
Markus Gasser <markus.gasser@konoma.ch>
2015-10-18 12:38:03 +0200

    Initial setup

---
+++ Sources/PromiseSignals.h
@@ -0,0 +1,17 @@
+//
+//  PromiseSignals.h
+//  promise-signals-ios
+//
+//  Created by Markus Gasser on 18.10.15.
+//  Copyright © 2015 konoma GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PromiseSignals.
+FOUNDATION_EXPORT double PromiseSignalsVersionNumber;
+
+//! Project version string for PromiseSignals.
+FOUNDATION_EXPORT const unsigned char PromiseSignalsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromiseSignals/PublicHeader.h>
---
+++ Tests/PromiseSignalsTests.swift
@@ -0,0 +1,13 @@
+//
+//  promise_signals_iosTests.swift
+//  promise-signals-iosTests
+//
+//  Created by Markus Gasser on 18.10.15.
+//  Copyright © 2015 konoma GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import PromiseSignals
+
+class PromiseSignalsTests: XCTestCase {
+}
