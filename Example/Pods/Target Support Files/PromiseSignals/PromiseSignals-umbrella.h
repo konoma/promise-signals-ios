99fb40ea4a67ada6cdb481fe7e85cbe5db22d744
Drew Pomerleau <dpomerleau@paypalcorp.com>
2016-12-09 11:03:27 -0500

    add example, add back pods

---
+++ Example/Pods/Target Support Files/PromiseSignals/PromiseSignals-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double PromiseSignalsVersionNumber;
+FOUNDATION_EXPORT const unsigned char PromiseSignalsVersionString[];
+
--- Example/PromiseSignals/ViewController.swift
+++ Example/PromiseSignals/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 import PromiseSignals
 
 class ViewController: UIViewController {
+    @IBOutlet var positiveCounterLabel: UILabel?
+    @IBOutlet var negativeCounterLabel: UILabel?
+    @IBOutlet var numberToSendTextField: UITextField?
+
+    var positiveLabelValue: Int {
+        get {
+            return optionalStringToInteger(positiveCounterLabel?.text)
+        }
+        set {
+            positiveCounterLabel?.text = String(newValue)
+        }
+    }
+
+    var negativeLabelValue: Int {
+        get {
+            return optionalStringToInteger(negativeCounterLabel?.text)
+        }
+        set {
+            negativeCounterLabel?.text = String(newValue)
+        }
+    }
+
+    var numberToSend: Int {
+        return optionalStringToInteger(numberToSendTextField?.text)
+    }
+
+    let signalObserver = SignalObserver()
+    let signalControl = SignalControl<Int>()
+    var signal: Signal<Int> { return signalControl.signal }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        signalObserver.observe(signal).then { [weak self] value in
+            self?.incrementPositiveCounter(change: value)
+        }
+
+        signalObserver.observe(signal).then { [weak self] value in
+            self?.decrementNegativeCounter(change: value)
+        }
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +58,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendSignalButtonTapped() {
+        signalControl.notifyValue(numberToSend)
+    }
+
+    func incrementPositiveCounter(change: Int) {
+        positiveLabelValue += change
+    }
+
+    func decrementNegativeCounter(change: Int) {
+        negativeLabelValue -= change
+    }
+
+    func optionalStringToInteger(_ str: String?, defaultValue: Int = 1) -> Int {
+        guard let str = str, let numValue = Int(str) else {
+            return defaultValue
+        }
+
+        return numValue
+    }
 }
 
