568a3250853918412e4ea510981c26074ab2bb0f
Markus Gasser <markus.gasser@konoma.ch>
2016-12-16 17:50:37 +0100

    Use Carthage as dependency management

---
+++ PromiseSignals/PromiseSignals.h
@@ -0,0 +1,16 @@
+//
+//  PromiseSignals.h
+//  PromiseSignals
+//
+//  Created by Markus Gasser on 16.12.16.
+//  Copyright © 2016 konoma GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//! Project version number for PromiseSignals.
+FOUNDATION_EXPORT double PromiseSignalsVersionNumber;
+
+//! Project version string for PromiseSignals.
+FOUNDATION_EXPORT const unsigned char PromiseSignalsVersionString[];
